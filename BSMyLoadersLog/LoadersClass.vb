b1a260636a29054977b6910227e028ebdc684cc0
BurnSoft <bs.github@burnsoft.net>
2026-02-18 10:46:48 -0500

    #48 Marked more function out

--- BSMyLoadersLog/LoadersClass.vb
+++ BSMyLoadersLog/LoadersClass.vb
@@ -308,87 +308,106 @@ Namespace LoadersClass
             Return Table
         End Function
     End Class
+    <Obsolete("Replaced with BurnSoft.Universal.BSFileSystem Class")>
     Public Class BSFileSystem
+        <Obsolete("Replaced with BurnSoft.Universal.BSFileSystem Class")>
         Public Sub LogFile(ByVal strPath As String, ByVal strMessage As String)
             Dim SendMessage As String = DateTime.Now & vbTab & strMessage
             Call AppendToFile(strPath, SendMessage)
             MDIParentMain.tsslErrorsFound.Visible = True
             MDIParentMain.tsslErrorsFound.Enabled = True
         End Sub
+        <Obsolete("Replaced with BurnSoft.Universal.BSFileSystem Class")>
         Public Sub DeleteFile(ByVal strPath As String)
             If File.Exists(strPath) Then
                 File.Delete(strPath)
             End If
         End Sub
+        <Obsolete("Replaced with BurnSoft.Universal.BSFileSystem Class")>
         Public Function FileExists(ByVal strPath As String)
             Return File.Exists(strPath)
         End Function
+        <Obsolete("Replaced with BurnSoft.Universal.BSFileSystem Class")>
         Private Sub CreateFile(ByVal strPath As String)
             If File.Exists(strPath) = False Then
                 Dim fs As New FileStream(strPath, FileMode.Append, FileAccess.Write, FileShare.Write)
                 fs.Close()
             End If
         End Sub
+        <Obsolete("Replaced with BurnSoft.Universal.BSFileSystem Class")>
         Private Sub AppendToFile(ByVal strPath As String, ByVal strNewLine As String)
             If File.Exists(strPath) = False Then Call CreateFile(strPath)
             Dim sw As New StreamWriter(strPath, True, Encoding.ASCII)
             sw.WriteLine(strNewLine)
             sw.Close()
         End Sub
+        <Obsolete("Replaced with BurnSoft.Universal.BSFileSystem Class")>
         Public Sub OutPutToFile(ByVal strPath As String, ByVal strNewLine As String)
             Call AppendToFile(strPath, strNewLine)
         End Sub
+        <Obsolete("Replaced with BurnSoft.Universal.BSFileSystem Class")>
         Public Sub MoveFile(ByVal strFrom As String, ByVal strTo As String)
             If File.Exists(strFrom) Then
                 File.Move(strFrom, strTo)
             End If
         End Sub
+        <Obsolete("Replaced with BurnSoft.Universal.BSFileSystem Class")>
         Public Sub CopyFile(ByVal strFrom As String, ByVal strTo As String)
             If File.Exists(strFrom) Then
                 File.Copy(strFrom, strTo)
             End If
         End Sub
+        <Obsolete("Replaced with BurnSoft.Universal.BSFileSystem Class")>
         Public Sub CreateDirectory(ByVal strPath As String)
             If Directory.Exists(strPath) Then
                 Directory.CreateDirectory(strPath)
             End If
         End Sub
+        <Obsolete("Replaced with BurnSoft.Universal.BSFileSystem Class")>
         Public Function DirectoryExists(ByVal strPath As String) As Boolean
             Return Directory.Exists(strPath)
         End Function
+        <Obsolete("Replaced with BurnSoft.Universal.BSFileSystem Class")>
         Public Sub DeleteDirectory(ByVal strPath As String)
             If Directory.Exists(strPath) Then
                 Directory.Delete(strPath)
             End If
         End Sub
+        <Obsolete("Replaced with BurnSoft.Universal.BSFileSystem Class")>
         Public Sub MoveDirectory(ByVal strFrom As String, ByVal strTo As String)
             If Directory.Exists(strFrom) Then
                 Directory.Move(strFrom, strTo)
             End If
         End Sub
+        <Obsolete("Replaced with BurnSoft.Universal.BSFileSystem Class")>
         Public Sub RenameFile(ByVal strFrom As String, ByVal strTo As String)
             File.Move(strFrom, strTo)
         End Sub
+        <Obsolete("Replaced with BurnSoft.Universal.BSFileSystem Class")>
         Public Function GetPathOfFile(ByVal strFile As String) As String
             Dim sAns As String = ""
             sAns = Path.GetDirectoryName(strFile)
             Return sAns
         End Function
+        <Obsolete("Replaced with BurnSoft.Universal.BSFileSystem Class")>
         Public Function GetExtOfFile(ByVal strFile As String) As String
             Dim sAns As String = ""
             sAns = Path.GetExtension(strFile)
             Return sAns
         End Function
+        <Obsolete("Replaced with BurnSoft.Universal.BSFileSystem Class")>
         Public Function GetNameOfFile(ByVal strFile As String) As String
             Dim sAns As String = ""
             sAns = Path.GetFileName(strFile)
             Return sAns
         End Function
+        <Obsolete("Replaced with BurnSoft.Universal.BSFileSystem Class")>
         Public Function FileHasExtension(ByVal strFile As String) As Boolean
             Dim bAns As Boolean = False
             bAns = Path.HasExtension(strFile)
             Return bAns
         End Function
+        <Obsolete("Replaced with BurnSoft.Universal.BSFileSystem Class")>
         Public Function GetNameOfFileWOExt(ByVal strFile As String) As String
             Dim sAns As String = ""
             sAns = Path.GetFileNameWithoutExtension(strFile)
