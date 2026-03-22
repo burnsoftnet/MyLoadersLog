cf4b9d83cc23e74bd90b839c594bcb579b2111ee
BurnSoft <bs.github@burnsoft.net>
2026-03-22 13:49:47 -0400

    #18 Added Commets to login windows

--- BSMyLoadersLog/frmLogin.vb
+++ BSMyLoadersLog/frmLogin.vb
@@ -1,5 +1,13 @@
-Public Class frmLogin
-    Private Sub OK_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OK.Click
+''' <summary>
+''' Form Login Windows Code
+''' </summary>
+Public Class FrmLogin
+    ''' <summary>
+    ''' Handles the Click event of the OK control.
+    ''' </summary>
+    ''' <param name="sender">The source of the event.</param>
+    ''' <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+    Private Sub OK_Click(ByVal sender As Object, ByVal e As EventArgs) Handles OK.Click
         Dim strUID As String = LCase(UsernameTextBox.Text)
         Dim strPWD As String = LCase(PasswordTextBox.Text)
         If strUID = LCase(UseMyUid) Then
@@ -8,23 +16,31 @@ Public Class frmLogin
                 MDIParentMain.Show()
                 Me.Close()
             Else
-                MsgBox("Incorrect Username/Password", MsgBoxStyle.Critical, Me.Text)
+                MsgBox("Incorrect Username/Password", MsgBoxStyle.Critical, Text)
                 UsernameTextBox.Text = ""
                 PasswordTextBox.Text = ""
             End If
         Else
-            MsgBox("Incorrect Username/Password", MsgBoxStyle.Critical, Me.Text)
+            MsgBox("Incorrect Username/Password", MsgBoxStyle.Critical, Text)
             UsernameTextBox.Text = ""
             PasswordTextBox.Text = ""
         End If
     End Sub
-
-    Private Sub Cancel_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Cancel.Click
-        Global.System.Windows.Forms.Application.Exit()
+    ''' <summary>
+    ''' Handles the Click event of the Cancel control.
+    ''' </summary>
+    ''' <param name="sender">The source of the event.</param>
+    ''' <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+    Private Sub Cancel_Click(ByVal sender As Object, ByVal e As EventArgs) Handles Cancel.Click
+        Application.Exit()
     End Sub
-
-    Private Sub Label1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label1.Click
-        Me.TopMost = False
+    ''' <summary>
+    ''' Handles the Click event of the Label1 control.
+    ''' </summary>
+    ''' <param name="sender">The source of the event.</param>
+    ''' <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+    Private Sub Label1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles Label1.Click
+        TopMost = False
         frmForgotPassword.Show()
     End Sub
 End Class
