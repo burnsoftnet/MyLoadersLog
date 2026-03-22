a657d8c0d2702289d70cc603b6bd47b97eeb3961
BurnSoft <bs.github@burnsoft.net>
2026-03-22 12:10:57 -0400

    #19 Added code form library

--- BSMyLoadersLog/Adding/frmAddFirearm.vb
+++ BSMyLoadersLog/Adding/frmAddFirearm.vb
@@ -1,10 +1,14 @@
-Imports BSMyLoadersLog.LoadersClass
 Imports BurnSoft.Applications.MLL.Helpers
+Imports BurnSoft.Applications.MLL.LoadersLog
+'Imports BSMyLoadersLog.LoadersClass
+'Imports BurnSoft.Applications.MLL.Types
 
 Namespace Adding
 
     Public Class FrmAddFirearm
+        Dim errOut as String
         Public FromView As Boolean
+        'TODO: #20 Clean Up Code
         'Sub AutoFill()
         '    Try
         '        'Dim ObjAF As New AutoFillCollections
@@ -24,7 +28,8 @@ Namespace Adding
                 Dim strCal As String = GeneralHelpers.FluffContent(txtCal.Text)
                 Dim strBarrel As String = GeneralHelpers.FluffContent(txtBarrel.Text)
                 Dim strType As String = GeneralHelpers.FluffContent(txtType.Text)
-                Dim MGCID As Integer = 0
+                'TODO: #20 Clean Up Code
+                'Dim MGCID As Integer = 0
                 Dim iExclude As Integer = 0
                 If chkExlude.Checked Then iExclude = 1
 
@@ -34,15 +39,18 @@ Namespace Adding
                 If Not GeneralHelpers.IsRequired(strCal, "Caliber", Text) Then Exit Sub
                 If Not GeneralHelpers.IsRequired(strType, "Type", Text) Then Exit Sub
 
-                Dim strFullName As String = strManu & " " & strModel
-                Dim Obj As New BSDatabase
-                Dim SQL As String = "INSERT INTO Loaders_Log_Firearms (MGCID,FullName,Manu,Model,Cal,Barrel,SerialNo,GType,exclude)" & _
-                                    " VALUES (" & MGCID & ",'" & strFullName & "','" & strManu & "','" & _
-                                    strModel & "','" & strCal & "','" & strBarrel & "','" & strSerial & _
-                                    "','" & strType & "'" & iExclude & ")"
-                Obj.ConnExec(SQL)
-                MsgBox(strFullName & " was added to the database!")
-                Me.Close()
+                'Dim strFullName As String = strManu & " " & strModel
+                If Not Firearms.Add(DatabasePath, strManu, strModel, strSerial, strCal, 
+                                    strType, strBarrel, errOut) Then Throw New Exception(errOut)
+                'TODO: #20 Clean Up Code
+                'Dim Obj As New BSDatabase
+                'Dim SQL As String = "INSERT INTO Loaders_Log_Firearms (MGCID,FullName,Manu,Model,Cal,Barrel,SerialNo,GType,exclude)" & _
+                '                    " VALUES (" & MGCID & ",'" & strFullName & "','" & strManu & "','" & _
+                '                    strModel & "','" & strCal & "','" & strBarrel & "','" & strSerial & _
+                '                    "','" & strType & "'" & iExclude & ")"
+                'Obj.ConnExec(SQL)
+                MsgBox(strManu & " " & strModel & " was added to the database!")
+                Close()
             Catch ex As Exception
                 Call LogError(Me.Name, "btnAdd.Click", Err.Number, ex.Message.ToString)
             End Try
