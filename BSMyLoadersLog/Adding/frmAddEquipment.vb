95fbac246325b7140625eec6bafaea7687e71dc6
BurnSoft <bs.github@burnsoft.net>
2026-03-22 11:55:59 -0400

    #18 added comments

--- BSMyLoadersLog/Adding/frmAddEquipment.vb
+++ BSMyLoadersLog/Adding/frmAddEquipment.vb
@@ -1,13 +1,27 @@
-Imports BSMyLoadersLog.LoadersClass
+'Imports BSMyLoadersLog.LoadersClass
 Imports BurnSoft.Applications.MLL.AutoFill
 Imports BurnSoft.Applications.MLL.Helpers
 Imports BurnSoft.Applications.MLL.Inventory
 
 Namespace Adding
-
+    ''' <summary>
+    ''' Class frmAddEquipment.
+    ''' Implements the <see cref="System.Windows.Forms.Form" />
+    ''' </summary>
+    ''' <seealso cref="System.Windows.Forms.Form" />
     Public Class frmAddEquipment
-        Dim errOut as String
+        ''' <summary>
+        ''' The error out
+        ''' </summary>
+        Dim _errOut as String
+        ''' <summary>
+        ''' From view
+        ''' </summary>
         Public FromView As Boolean
+        ''' <summary>
+        ''' Automatics the fill.
+        ''' </summary>
+        ''' <exception cref="System.Exception"></exception>
         Sub AutoFill()
             Try
                 'TODO #20 Delete Old Code
@@ -17,27 +31,37 @@ Namespace Adding
                 'txtUse.AutoCompleteCustomSource = ObjAF.General_Equipment_Use
                 'txtPrice.AutoCompleteCustomSource = ObjAF.General_Equipment_Cost
                 'Dim ObjAF As New AutoFillCollections
-                txtManu.AutoCompleteCustomSource = Equipment.Manufacturer(DatabasePath, errOut)
-                If errOut.Length > 0 Then Throw New Exception(errOut)
-                txtName.AutoCompleteCustomSource = Equipment.Name(DatabasePath, errOut)
-                If errOut.Length > 0 Then Throw New Exception(errOut)
-                txtUse.AutoCompleteCustomSource = Equipment.Use(DatabasePath, errOut)
-                If errOut.Length > 0 Then Throw New Exception(errOut)
-                txtPrice.AutoCompleteCustomSource = Equipment.Cost(DatabasePath, errOut)
-                If errOut.Length > 0 Then Throw New Exception(errOut)
+                txtManu.AutoCompleteCustomSource = Equipment.Manufacturer(DatabasePath, _errOut)
+                If _errOut.Length > 0 Then Throw New Exception(_errOut)
+                txtName.AutoCompleteCustomSource = Equipment.Name(DatabasePath, _errOut)
+                If _errOut.Length > 0 Then Throw New Exception(_errOut)
+                txtUse.AutoCompleteCustomSource = Equipment.Use(DatabasePath, _errOut)
+                If _errOut.Length > 0 Then Throw New Exception(_errOut)
+                txtPrice.AutoCompleteCustomSource = Equipment.Cost(DatabasePath, _errOut)
+                If _errOut.Length > 0 Then Throw New Exception(_errOut)
             Catch ex As Exception
                 Call LogError(Me.Name, "AutoFill", Err.Number, ex.Message.ToString)
             End Try
         End Sub
-
+        ''' <summary>
+        ''' Handles the Load event of the frmAddEquipment control.
+        ''' </summary>
+        ''' <param name="sender">The source of the event.</param>
+        ''' <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
         Private Sub frmAddEquipment_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
             Call AutoFill()
         End Sub
-
+        ''' <summary>
+        ''' Handles the Click event of the btnCancel control.
+        ''' </summary>
+        ''' <param name="sender">The source of the event.</param>
+        ''' <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
         Private Sub btnCancel_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCancel.Click
             Me.Close()
         End Sub
-
+        ''' <summary>
+        ''' Clears the fields.
+        ''' </summary>
         Private Sub ClearFields()
             txtManu.Text = ""
             txtName.Text = ""
@@ -45,13 +69,18 @@ Namespace Adding
             txtPrice.Text = ""
             txtNotes.Text = ""
         End Sub
-
+        ''' <summary>
+        ''' Handles the Click event of the btnAdd control.
+        ''' </summary>
+        ''' <param name="sender">The source of the event.</param>
+        ''' <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
+        ''' <exception cref="System.Exception"></exception>
         Private Sub btnAdd_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnAdd.Click
             Try
                 Dim strManu As String = GeneralHelpers.FluffContent(txtManu.Text)
                 Dim strName As String = GeneralHelpers.FluffContent(txtName.Text)
                 Dim strUse As String = GeneralHelpers.FluffContent(txtUse.Text)
-                Dim strPrice As Double = GeneralHelpers.FluffContent(CDbl(txtPrice.Text), 0)
+                Dim strPrice As Double = GeneralHelpers.FluffContent(txtPrice.Text, 0)
                 Dim strNotes As String = GeneralHelpers.FluffContent(txtNotes.Text)
 
                 If Not GeneralHelpers.IsRequired(strManu, "Manufacturer", Me.Text) Then Exit Sub
@@ -67,7 +96,7 @@ Namespace Adding
                 'Obj = Nothing
 
                 If Not EquipmentInventory.Add(DatabasePath, strManu, strName, strUse, strPrice, 
-                                              strNotes, errOut) Then Throw New Exception(errOut)
+                                              strNotes, _errOut) Then Throw New Exception(_errOut)
 
                 Dim sAns As String = MsgBox("Equipment was added to the database, do you with to add another?", MsgBoxStyle.YesNo, Me.Text)
                 If FromView Then Call frmView_List_Equipment.LoadData()
