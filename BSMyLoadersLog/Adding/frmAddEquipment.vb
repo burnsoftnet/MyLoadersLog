4d133dc9dca2aca3d67f53570b8668eeac13e48b
BurnSoft <bs.github@burnsoft.net>
2026-03-22 11:54:20 -0400

    #19 REFACTOR COde to use library

--- BSMyLoadersLog/Adding/frmAddEquipment.vb
+++ BSMyLoadersLog/Adding/frmAddEquipment.vb
@@ -1,17 +1,30 @@
 Imports BSMyLoadersLog.LoadersClass
+Imports BurnSoft.Applications.MLL.AutoFill
 Imports BurnSoft.Applications.MLL.Helpers
+Imports BurnSoft.Applications.MLL.Inventory
 
 Namespace Adding
 
     Public Class frmAddEquipment
+        Dim errOut as String
         Public FromView As Boolean
         Sub AutoFill()
             Try
-                Dim ObjAF As New AutoFillCollections
-                txtManu.AutoCompleteCustomSource = ObjAF.General_Equipment_Manufacturer
-                txtName.AutoCompleteCustomSource = ObjAF.General_Equipment_Name
-                txtUse.AutoCompleteCustomSource = ObjAF.General_Equipment_Use
-                txtPrice.AutoCompleteCustomSource = ObjAF.General_Equipment_Cost
+                'TODO #20 Delete Old Code
+                'Dim ObjAF As New AutoFillCollections
+                'txtManu.AutoCompleteCustomSource = ObjAF.General_Equipment_Manufacturer
+                'txtName.AutoCompleteCustomSource = ObjAF.General_Equipment_Name
+                'txtUse.AutoCompleteCustomSource = ObjAF.General_Equipment_Use
+                'txtPrice.AutoCompleteCustomSource = ObjAF.General_Equipment_Cost
+                'Dim ObjAF As New AutoFillCollections
+                txtManu.AutoCompleteCustomSource = Equipment.Manufacturer(DatabasePath, errOut)
+                If errOut.Length > 0 Then Throw New Exception(errOut)
+                txtName.AutoCompleteCustomSource = Equipment.Name(DatabasePath, errOut)
+                If errOut.Length > 0 Then Throw New Exception(errOut)
+                txtUse.AutoCompleteCustomSource = Equipment.Use(DatabasePath, errOut)
+                If errOut.Length > 0 Then Throw New Exception(errOut)
+                txtPrice.AutoCompleteCustomSource = Equipment.Cost(DatabasePath, errOut)
+                If errOut.Length > 0 Then Throw New Exception(errOut)
             Catch ex As Exception
                 Call LogError(Me.Name, "AutoFill", Err.Number, ex.Message.ToString)
             End Try
@@ -23,6 +36,14 @@ Namespace Adding
 
         Private Sub btnCancel_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCancel.Click
             Me.Close()
+        End Sub
+
+        Private Sub ClearFields()
+            txtManu.Text = ""
+            txtName.Text = ""
+            txtUse.Text = ""
+            txtPrice.Text = ""
+            txtNotes.Text = ""
         End Sub
 
         Private Sub btnAdd_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnAdd.Click
@@ -38,19 +59,20 @@ Namespace Adding
                 If Not GeneralHelpers.IsRequired(strUse, "Use", Me.Text) Then Exit Sub
                 If Not GeneralHelpers.IsRequired(strPrice, 0, "Price", Me.Text) Then Exit Sub
 
-                Dim Obj As New BSDatabase
-                Dim SQL As String = "INSERT INTO General_Equipment(Manufacturer,Name,Use,Cost,Notes) VALUES('" & _
-                                    strManu & "','" & strName & "','" & strUse & "'," & strPrice & ",'" & strNotes & "')"
-                Obj.ConnExec(SQL)
-                Obj = Nothing
+                'TODO #20 Delete Old Code
+                'Dim Obj As New BSDatabase
+                'Dim SQL As String = "INSERT INTO General_Equipment(Manufacturer,Name,Use,Cost,Notes) VALUES('" & _
+                '                    strManu & "','" & strName & "','" & strUse & "'," & strPrice & ",'" & strNotes & "')"
+                'Obj.ConnExec(SQL)
+                'Obj = Nothing
+
+                If Not EquipmentInventory.Add(DatabasePath, strManu, strName, strUse, strPrice, 
+                                              strNotes, errOut) Then Throw New Exception(errOut)
+
                 Dim sAns As String = MsgBox("Equipment was added to the database, do you with to add another?", MsgBoxStyle.YesNo, Me.Text)
                 If FromView Then Call frmView_List_Equipment.LoadData()
                 If sAns = vbYes Then
-                    txtManu.Text = ""
-                    txtName.Text = ""
-                    txtUse.Text = ""
-                    txtPrice.Text = ""
-                    txtNotes.Text = ""
+                    ClearFields()
                 Else
                     Me.Close()
                 End If
