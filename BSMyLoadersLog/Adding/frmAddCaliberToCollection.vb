1a51b6064169a43bcf7fc5ef99fe83c40eff14ef
BurnSoft <bs.github@burnsoft.net>
2026-03-22 10:51:41 -0400

    #20 Deleted Old Code and Tested Functions in adding Forms

--- BSMyLoadersLog/Adding/frmAddCaliberToCollection.vb
+++ BSMyLoadersLog/Adding/frmAddCaliberToCollection.vb
@@ -12,7 +12,7 @@ Namespace Adding
         ''' <summary>
         ''' The error out
         ''' </summary>
-        Private errOut as String
+        Private _errOut as String
         ''' <summary>
         ''' Handles the Load event of the frmAddCaliberToCollection control.
         ''' </summary>
@@ -21,16 +21,12 @@ Namespace Adding
         ''' <exception cref="System.Exception"></exception>
         Private Sub frmAddCaliberToCollection_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
             Try
-                ' TODO: #20 CLEAN UP CODE
-                'Dim objAf As New AutoFillCollections
-                'txtCal.AutoCompleteSource = AutoCompleteSource.CustomSource
-                'txtCal.AutoCompleteCustomSource = objAf.General_Calibers
-                'Dim objAf As New AutoFillCollections
                 txtCal.AutoCompleteSource = AutoCompleteSource.CustomSource
-                txtCal.AutoCompleteCustomSource = Calibers.ShowAll(DatabasePath, errOut)
-                If errOut.Length > 0 Then Throw New Exception(errOut)
+                txtCal.AutoCompleteCustomSource = Calibers.ShowAll(DatabasePath, _errOut)
+                If _errOut.Length > 0 Then Throw New Exception(_errOut)
             Catch ex As Exception
-                Call LogError(Name, "frmAddCaliberToCollection_Load", Err.Number, ex.Message.ToString)
+                Call LogError(Name, "frmAddCaliberToCollection_Load", 
+                              Err.Number, ex.Message.ToString)
             End Try
         End Sub
         ''' <summary>
@@ -49,21 +45,9 @@ Namespace Adding
         ''' <exception cref="System.Exception"></exception>
         Private Sub btnAdd_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnAdd.Click
             Try
-                ' TODO: #20 CLEAN UP CODE
-                'Dim strCal As String = FluffContent(txtCal.Text)
-                'If Not IsRequired(strCal, "Caliber", Text) Then Exit Sub
-                'Dim obj As New BSDatabase
-                'Dim sql As String = "INSERT INTO List_Calibers(CAL) VALUES('" & strCal & "')"
-                'obj.ConnExec(sql)
-                'MDIParentMain.RefreshCalData()
-                'If Not chkKeep.Checked Then
-                '    Close()
-                'Else
-                '    txtCal.Text = ""
-                'End If
                 Dim strCal As String = GeneralHelpers.FluffContent(txtCal.Text, "  ")
                 If Not GeneralHelpers.IsRequired(strCal, "Caliber", Text) Then Exit Sub
-                If Not CaliberInventory.Add(DatabasePath, strCal, errOut) Then Throw New Exception(errOut)
+                If Not CaliberInventory.Add(DatabasePath, strCal, _errOut) Then Throw New Exception(_errOut)
             
                 MDIParentMain.RefreshCalData()
                 If Not chkKeep.Checked Then
@@ -72,7 +56,8 @@ Namespace Adding
                     txtCal.Text = ""
                 End If
             Catch ex As Exception
-                Call LogError(Name, "btnAdd_Click", Err.Number, ex.Message.ToString)
+                Call LogError(Name, "btnAdd_Click", Err.Number, 
+                              ex.Message.ToString)
             End Try
         End Sub
     End Class
