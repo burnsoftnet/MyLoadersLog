d17cb9e48f3f1a0cc7d259b0d4d71c6b6f35bd52
BurnSoft <bs.github@burnsoft.net>
2026-03-24 11:39:39 -0400

    #18 Added Comments

--- BSMyLoadersLog/Adding/frmAddQtyBullets.vb
+++ BSMyLoadersLog/Adding/frmAddQtyBullets.vb
@@ -6,10 +6,23 @@ Imports BurnSoft.Applications.MLL.Inventory
 Imports BurnSoft.Applications.MLL.Types
 
 Namespace Adding
-
+    ''' <summary>
+    ''' Class FrmAddQtyBullets.
+    ''' Implements the <see cref="System.Windows.Forms.Form" />
+    ''' </summary>
+    ''' <seealso cref="System.Windows.Forms.Form" />
     Public Class FrmAddQtyBullets
+        ''' <summary>
+        ''' The error out
+        ''' </summary>
         Dim _errOut As String
+        ''' <summary>
+        ''' The bullet identifier
+        ''' </summary>
         Public BulletId As Long
+        ''' <summary>
+        ''' From view
+        ''' </summary>
         Public FromView As Boolean
         'Function PricePerItem(ByVal lQty As Long, ByVal dPrice As Double) As Double
         '    Dim dAns As Double = 0
@@ -19,7 +32,11 @@ Namespace Adding
         '    End If
         '    ObjIM.ConvertToDollars(dAns)
         '    Return dAns
-        'End Function
+        'End Function        
+        ''' <summary>
+        ''' Loads the data.
+        ''' </summary>
+        ''' <exception cref="System.Exception"></exception>
         Sub LoadData()
             Try
                 Dim lst as List(Of BulletListings) = BulletsInventory.GetDetails(DatabasePath, BulletId, _errOut)
@@ -57,6 +74,10 @@ Namespace Adding
                 Call LogError(Name, "LoadData", Err.Number, ex.Message.ToString)
             End Try
         End Sub
+        ''' <summary>
+        ''' Saves the data.
+        ''' </summary>
+        ''' <exception cref="System.Exception"></exception>
         Sub SaveData()
             Try
                 Dim currentQty As Long = CLng(GeneralHelpers.FluffContent(txtCQty.Text, 0))
@@ -89,15 +110,35 @@ Namespace Adding
                 Call LogError(Name, "SaveData", Err.Number, ex.Message.ToString)
             End Try
         End Sub
+        ''' <summary>
+        ''' Handles the Load event of the frmAddQtyBullets control.
+        ''' </summary>
+        ''' <param name="sender">The source of the event.</param>
+        ''' <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
         Private Sub frmAddQtyBullets_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
             Call LoadData()
         End Sub
+        ''' <summary>
+        ''' Handles the Click event of the btnCancel control.
+        ''' </summary>
+        ''' <param name="sender">The source of the event.</param>
+        ''' <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
         Private Sub btnCancel_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnCancel.Click
             Close()
         End Sub
+        ''' <summary>
+        ''' Handles the Click event of the btnViewCalc control.
+        ''' </summary>
+        ''' <param name="sender">The source of the event.</param>
+        ''' <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
         Private Sub btnViewCalc_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnViewCalc.Click
             txtUPPI.Text = CDbl(txtUPrice.Text) / CLng(txtUQty.Text)
         End Sub
+        ''' <summary>
+        ''' Handles the Click event of the btnUpdate control.
+        ''' </summary>
+        ''' <param name="sender">The source of the event.</param>
+        ''' <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
         Private Sub btnUpdate_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnUpdate.Click
             Try
                 Call SaveData()
