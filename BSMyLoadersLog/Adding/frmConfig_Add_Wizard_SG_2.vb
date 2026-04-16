d4313c3de9b16943a374e9829122c6ad4b57d774
BurnSoft <bs.github@burnsoft.net>
2026-04-16 11:03:30 -0400

    #19 updated ui with new functions

--- BSMyLoadersLog/Adding/frmConfig_Add_Wizard_SG_2.vb
+++ BSMyLoadersLog/Adding/frmConfig_Add_Wizard_SG_2.vb
@@ -1,8 +1,11 @@
-Imports System.Data
-Imports System.Data.Odbc
-Imports BSMyLoadersLog.LoadersClass
+'Imports System.Data
+'Imports System.Data.Odbc
+'Imports BSMyLoadersLog.LoadersClass
 Imports BurnSoft.Applications.MLL.AutoFill
+Imports BurnSoft.Applications.MLL.ConfigSheets
 Imports BurnSoft.Applications.MLL.Helpers
+Imports BurnSoft.Applications.MLL.Inventory
+Imports BurnSoft.Applications.MLL.Types
 
 Namespace Adding
     ''' <summary>
@@ -14,7 +17,7 @@ Namespace Adding
         ''' <summary>
         ''' The error out
         ''' </summary>
-        Dim errOut as String
+        Dim _errOut As String
         ''' <summary>
         ''' The configuration name
         ''' </summary>
@@ -47,17 +50,17 @@ Namespace Adding
                 List_SG_CaseTableAdapter.FillBy_CFG_List(MLLDataSet.List_SG_Case, GaugeId)
                 List_SG_WADTableAdapter.FillBy_CFG_WADList(MLLDataSet.List_SG_WAD, GaugeId)
                 ViewPrimerListTableAdapter.FillBy_CFG_List(MLLDataSet.viewPrimerList)
-                Dim WID As Long = cmdWAD.SelectedValue
-                txtShotCharge.Text = GetMaxWADCharge(WID)
+                Dim wadId As Long = cmdWAD.SelectedValue
+                txtShotCharge.Text = GetMaxWADCharge(wadId)
                 List_SG_ShotCharge_LoadsTableAdapter.Fill(MLLDataSet.List_SG_ShotCharge_Loads)
                 'Dim ObjAF As New AutoFillCollections.ShotGun
                 'txtSource.AutoCompleteCustomSource = ObjAF.Config_Source_SG
                 'txtShotCharge.AutoCompleteCustomSource = ObjAF.Config_LoadInOZ_SG
                 'Dim ObjAF As New AutoFillCollections.ShotGun
-                txtSource.AutoCompleteCustomSource = ConfigShotgun.Source(DatabasePath, errOut)
-                If errOut.Length > 0 Then Throw New Exception(errOut)
-                txtShotCharge.AutoCompleteCustomSource = ConfigShotgun.LoadInOunces(DatabasePath, errOut)
-                If errOut.Length > 0 Then Throw New Exception(errOut)
+                txtSource.AutoCompleteCustomSource = ConfigShotgun.Source(DatabasePath, _errOut)
+                If _errOut.Length > 0 Then Throw New Exception(_errOut)
+                txtShotCharge.AutoCompleteCustomSource = ConfigShotgun.LoadInOunces(DatabasePath, _errOut)
+                If _errOut.Length > 0 Then Throw New Exception(_errOut)
             Catch ex As Exception
                 Call LogError(Name, "Load", Err.Number, ex.Message.ToString)
             End Try
@@ -67,22 +70,28 @@ Namespace Adding
         ''' </summary>
         ''' <param name="iID">The i identifier.</param>
         ''' <returns>System.String.</returns>
+        <Obsolete("Replaced with WadInventory.GetMaxCharge in 3.0.3.80-beta")>
         Function GetMaxWADCharge(ByVal iID As Long) As String
             Dim sAns As String = ""
             Try
-                Dim SQL As String = "SELECT * from List_SG_WAD where ID=" & iID
-                Dim Obj As New BSDatabase
-                Obj.ConnectDB()
-                Dim CMD As New OdbcCommand(SQL, Obj.Conn)
-                Dim RS As OdbcDataReader
-                RS = CMD.ExecuteReader
-                While RS.Read
-                    sAns = RS("load_t")
-                End While
-                RS.Close()
-                RS = Nothing
-                CMD = Nothing
-                Obj.CloseDB()
+                Dim lst As List(Of WadData) = WadInventory.GetDetails(DatabasePath, iID, _errOut)
+                If _errOut.Length > 0 Then Throw New Exception(_errOut)
+                For Each o As WadData In lst
+                    sAns = o.LoadInOzText
+                Next
+                'Dim SQL As String = "SELECT * from List_SG_WAD where ID=" & iID
+                'Dim Obj As New BSDatabase
+                'Obj.ConnectDB()
+                'Dim CMD As New OdbcCommand(Sql, Obj.Conn)
+                'Dim RS As OdbcDataReader
+                'RS = CMD.ExecuteReader
+                'While RS.Read
+                '    sAns = RS("load_t")
+                'End While
+                'RS.Close()
+                'RS = Nothing
+                'CMD = Nothing
+                'Obj.CloseDB()
             Catch ex As Exception
                 Call LogError(Name, "GetMaxWADCharge", Err.Number, ex.Message.ToString)
             End Try
@@ -94,8 +103,12 @@ Namespace Adding
         ''' <param name="sender">The source of the event.</param>
         ''' <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
         Private Sub cmdWAD_SelectedIndexChanged(ByVal sender As Object, ByVal e As EventArgs) Handles cmdWAD.SelectedIndexChanged
-            Dim WID As Long = cmdWAD.SelectedValue
-            txtShotCharge.Text = GetMaxWADCharge(WID)
+            Try
+                Dim wadId As Long = cmdWAD.SelectedValue
+                txtShotCharge.Text = GetMaxWADCharge(wadId)
+            Catch ex As Exception
+                Call LogError(Name, "cmdWAD_SelectedIndexChanged", Err.Number, ex.Message.ToString)
+            End Try
         End Sub
 
         ''' <summary>
@@ -105,26 +118,35 @@ Namespace Adding
         ''' <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
         Private Sub btnNext_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnNext.Click
             Try
-                Dim PID As Long = cmbPrimer.SelectedValue
-                Dim HID As Long = cmdHull.SelectedValue
-                Dim WID As Long = cmdWAD.SelectedValue
-                Dim sLoad As String = txtShotCharge.Text
-                Dim dLoad As Double = Converters.ConvertOZToDouble(sLoad, errOut)
-                Dim SLTID As Long = cmdShotType.SelectedValue
-                Dim LTID As Long = cmbLoadType.SelectedValue
-                Dim sSource As String = GeneralHelpers.FluffContent(txtSource.Text)
-                Dim IsPersonal As Boolean = chkPersonal.Checked
-                Dim iPersonal As Integer = 1
-                Dim SQL As String = ""
-                Dim Obj As New BSDatabase
-                If Not IsPersonal Then iPersonal = 0
-                SQL = "INSERT INTO Config_List_Data_SG (CLNID,ATID,CALID,PRID,CAID,SW,SW_t,WAD" & _
-                      ",SCL,Source,GID,IsPersonal,LTID) VALUES(" & ConfigId & "," & CaliberId & "," & GaugeId & "," & PID & _
-                      "," & HID & "," & dLoad & ",'" & sLoad & "'," & WID & "," & SLTID & ",'" & _
-                      sSource & "'," & GaugeId & "," & iPersonal & "," & LTID & ")"
-                Obj.ConnExec(SQL)
-                SQL = "UPDATE Config_List_Name set IsPersonal=" & iPersonal & " where id=" & ConfigId
-                Obj.ConnExec(SQL)
+                Dim primerId As Long = cmbPrimer.SelectedValue
+                Dim hullId As Long = cmdHull.SelectedValue
+                Dim wadId As Long = cmdWAD.SelectedValue
+                Dim shotCharge As String = txtShotCharge.Text
+                Dim shotChargeNumeric As Double = Converters.ConvertOzToDouble(shotCharge, _errOut)
+                Dim shotTypeId As Long = cmdShotType.SelectedValue
+                Dim loadTypeId As Long = cmbLoadType.SelectedValue
+                Dim source As String = GeneralHelpers.FluffContent(txtSource.Text)
+                ' TODO: Double check this
+                If Not ConfigListDataShotgun.Add(DatabasePath, ConfigId, CaliberId, 
+                                                 GaugeId, primerId, hullId, shotChargeNumeric, 
+                                                 shotCharge, shotTypeId, 0, wadId, 
+                                                 loadTypeId, source, GaugeId, chkPersonal.Checked, 
+                                                 loadTypeId, 0, 0, 
+                                                 _errOut) Then Throw new Exception(_errOut)
+                If Not ConfigListDataName.SetPersonal(DatabaseName, ConfigId, chkPersonal.Checked, 
+                                                      _errOut) Then Throw New Exception(_errOut)
+                    'Dim isPersonal As Boolean = chkPersonal.Checked
+                    'Dim iPersonal As Integer = 1
+                    'Dim sql As String = ""
+                    'Dim obj As New BSDatabase
+                    'If Not isPersonal Then iPersonal = 0
+                    'sql = "INSERT INTO Config_List_Data_SG (CLNID,ATID,CALID,PRID,CAID,SW,SW_t,WAD" & _
+                    '      ",SCL,Source,GID,IsPersonal,LTID) VALUES(" & ConfigId & "," & CaliberId & "," & GaugeId & "," & primerId & _
+                    '      "," & hullId & "," & shotChargeNumeric & ",'" & shotCharge & "'," & wadId & "," & shotTypeId & ",'" & _
+                    '      source & "'," & GaugeId & "," & iPersonal & "," & loadTypeId & ")"
+                    'obj.ConnExec(sql)
+                    'sql = "UPDATE Config_List_Name set IsPersonal=" & iPersonal & " where id=" & ConfigId
+                    'obj.ConnExec(sql)
                 frmConfig_Add_Wizard_SG_Powder.MdiParent = MdiParent
                 frmConfig_Add_Wizard_SG_Powder.ConfigID = ConfigId
                 frmConfig_Add_Wizard_SG_Powder.ConfigName = ConfigName
@@ -151,4 +173,4 @@ Namespace Adding
             End If
         End Sub
     End Class
-End NameSpace
+End Namespace
